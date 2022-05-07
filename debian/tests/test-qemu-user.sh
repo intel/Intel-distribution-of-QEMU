08640d08d50da103fd0af426f715686468dddf17
Michael Tokarev <mjt@tls.msk.ru>
2022-05-07 12:22:26 +0300

    1:7.0+dfsg-5 (patches unapplied)
    Imported using git-ubuntu import.

--- debian/tests/test-qemu-user.sh
+++ debian/tests/test-qemu-user.sh
@@ -7,8 +7,17 @@ dpkg-architecture -q DEB_HOST_GNU_CPU 2>&1
 
 # redirect it to /dev/null for actual run
 ARCH=$(dpkg-architecture -q DEB_HOST_GNU_CPU 2>/dev/null)
-echo DEB_HOST_GNU_CPU=$ARCH uname-m=$(uname -m) arch=$(arch)
+echo DEB_HOST_GNU_CPU=$ARCH uname-m=$(uname -m) arch=$(arch) dpkg-print-architecture: $(dpkg --print-architecture)
 
-echo -n "Checking if qemu-${ARCH} can run executables..."
-qemu-${ARCH} /bin/ls -1 debian/changelog | grep -qs ^debian/changelog
+case $ARCH in
+  amd64) ARCH=x86_64 ;;
+  i[456]86) ARCH=i386 ;;
+esac
+
+if [ -x /usr/bin/qemu-$ARCH ]; then
+echo "Checking if qemu-$ARCH can run executables:"
+qemu-$ARCH /bin/ls -1 debian/changelog | grep ^debian/changelog
 echo "done."
+else
+echo Warning: qemu-$ARCH not found, not testing qemu-user
+fi
