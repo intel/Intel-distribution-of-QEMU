5798ffb4adf59ab49dc591660f6a2aa5159ec665
Michael Tokarev <mjt@tls.msk.ru>
2022-05-09 01:37:56 +0300

    1:7.0+dfsg-6 (patches unapplied)
    Imported using git-ubuntu import.

--- debian/tests/test-qemu-user.sh
+++ debian/tests/test-qemu-user.sh
@@ -2,22 +2,33 @@
 
 set -e
 
-# dpkg-architecture wants gcc and prints a warning if not found
-dpkg-architecture -q DEB_HOST_GNU_CPU 2>&1
-
-# redirect it to /dev/null for actual run
-ARCH=$(dpkg-architecture -q DEB_HOST_GNU_CPU 2>/dev/null)
-echo DEB_HOST_GNU_CPU=$ARCH uname-m=$(uname -m) arch=$(arch) dpkg-print-architecture: $(dpkg --print-architecture)
-
-case $ARCH in
-  amd64) ARCH=x86_64 ;;
-  i[456]86) ARCH=i386 ;;
+arch=$(dpkg --print-architecture)
+echo "debian architecture: $arch"
+case $arch in
+  ( amd64 | x86_64 ) arch=x86_64 ;;
+  ( i[3456]86 ) arch=i386 ;;
+  ( arm64 | aarch64 ) arch=aarch64 ;;
+  ( arm | armel | armhf ) arch=arm ;;
+  ( ppc64el | powerpc64le ) arch=ppc64el ;;
+  ( s390x ) ;;
+  ( * ) echo "Warning: unmapped architecture $arch" ;;
 esac
+echo "qemu   architecture: $arch"
 
-if [ -x /usr/bin/qemu-$ARCH ]; then
-echo "Checking if qemu-$ARCH can run executables:"
-qemu-$ARCH /bin/ls -1 debian/changelog | grep ^debian/changelog
-echo "done."
-else
-echo Warning: qemu-$ARCH not found, not testing qemu-user
+tested=
+for f in qemu-$arch qemu-$arch-static ; do
+  [ -x /usr/bin/$f ] || continue
+  echo "Checking if $f can run executables:"
+  echo "glob with sh: $f /bin/sh -c '$f /bin/ls -ld debian/':"
+  ls="$($f /bin/sh -c "$f /bin/ls -ld debian/*")"
+  echo "$ls"
+  case "$ls" in
+    (*debian/control*) ;;
+    *) echo "Expected output not found" >&2; exit 1;;
+  esac
+  echo ok.
+  tested=y
+done
+if [ ! "$tested" ]; then
+  echo "Warning: qemu-$arch[-static] not found, not testing qemu-user"
 fi
