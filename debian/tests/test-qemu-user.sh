08589075a375141c6c62c04b3505cf23751a8406
Michael Tokarev <mjt@tls.msk.ru>
2022-05-15 15:49:12 +0300

    1:7.0+dfsg-7 (patches unapplied)
    Imported using git-ubuntu import.

--- debian/tests/test-qemu-user.sh
+++ debian/tests/test-qemu-user.sh
@@ -9,7 +9,7 @@ case $arch in
   ( i[3456]86 ) arch=i386 ;;
   ( arm64 | aarch64 ) arch=aarch64 ;;
   ( arm | armel | armhf ) arch=arm ;;
-  ( ppc64el | powerpc64le ) arch=ppc64el ;;
+  ( ppc64el | powerpc64le ) arch=ppc64le ;;
   ( s390x ) ;;
   ( * ) echo "Warning: unmapped architecture $arch" ;;
 esac
@@ -19,8 +19,8 @@ tested=
 for f in qemu-$arch qemu-$arch-static ; do
   [ -x /usr/bin/$f ] || continue
   echo "Checking if $f can run executables:"
-  echo "glob with sh: $f /bin/sh -c '$f /bin/ls -ld debian/':"
-  ls="$($f /bin/sh -c "$f /bin/ls -ld debian/*")"
+  echo "glob with sh: $f /bin/sh -c '$f /bin/ls -dCFl debian/*[t]*':"
+  ls="$($f /bin/sh -c "$f /bin/ls -dCFl debian/*[t]*")"
   echo "$ls"
   case "$ls" in
     (*debian/control*) ;;
