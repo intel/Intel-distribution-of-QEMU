f265f4788f9fa2c276d2d19f82a80cc3dd5639f8
Michael Tokarev <mjt@tls.msk.ru>
2024-09-06 13:18:51 +0300

    Revert "move helper binaries (qemu-bridge-helper, virtfs-proxy-helper, vhost-user-gpu) from usr/lib/qemu to usr/libexec/qemu"
    This reverts commit 1a6f15e50a79db855aa1ce942c3eaedd9398abb3.
    
    Move the binaries back there they were before this change.
    
    At the very least, we should resolve #765936 first, because
    some users enabled setuid bit for qemu-bridge-helper using
    dpkg-statoverride, and such rename breaks this override.
    
    Also, the whole change seems to be wrong, since other tools
    (eg libvirt) search for the helpers in /usr/libexec/ directly,
    not in a subdir there like it was in /usr/lib/qemu/.  See
    https://github.com/search?q=repo%3Alibvirt%2Flibvirt+lib%2Fqemu&type=code
    as an example.
    
    And even when moving things into the "rigth" place, libvirt
    will find our compat wrapper in /usr/lib/qemu/ first anyway,
    since it is looked before /usr/libexec/. And the output from
    our wrapper script isn't shown anywhere when run under libvirt,
    so it will just annoy users.
    
    Closes: #1080456

--- debian/lib2libexec.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# Compatibility wrapper for moving stuff from /usr/lib/ to /usr/libexec/.
-# Remove for trixie+1.
-
-n="/usr/libexec/qemu/${0##*/}"
-echo "W: $0 is deprecated, use $n instead" >&2
-sleep 10
-exec "$n" "$@"
