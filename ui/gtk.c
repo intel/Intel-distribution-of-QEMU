89f7054c968d96122fd6a70188c6835ac6c4fb19
Dongwon Kim <dongwon.kim@intel.com>
2024-10-29 19:17:49 -0700

    ui/gtk: Hardcode default size of new tab window to 960 by 540
    This is a WA to keep the display resolution from windows VM
    after waking up from S4.
    
    Signed-off-by: Dongwon Kim <dongwon.kim@intel.com>

--- ui/gtk.c
+++ ui/gtk.c
@@ -1574,9 +1574,13 @@ static gboolean gd_window_state_event(GtkWidget *widget, GdkEvent *event,
 static void gd_tab_window_create(VirtualConsole *vc)
 {
     GtkDisplayState *s = vc->s;
+    int default_width = 960;
+    int default_height = 540;
 
     gtk_widget_set_sensitive(vc->menu_item, false);
     vc->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(vc->window),
+                                default_width, default_height);
 #if defined(CONFIG_OPENGL)
     if (vc->gfx.esurface) {
         eglDestroySurface(qemu_egl_display, vc->gfx.esurface);
